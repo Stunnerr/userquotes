e36a4aa04de6fe07e9d3bb260a679c54074cb961
Stunner <semgol824@gmail.com>
2020-11-24 13:25:17 +0500

    added comments

--- ftgquotes.py
+++ ftgquotes.py
@@ -7,24 +7,28 @@ from telethon.errors.rpcerrorlist import ChannelPrivateError as PrivateChannel
 from .. import loader, utils
 
 @loader.tds
-class quotebuilderMod(loader.Module):
+class QuoteBuilderMod(loader.Module):
+    """
+    Generates quotes on userbot, without an API.
+    """
+
     arrow = '''
-    <svg preserveAspectRatio="XMinYMin slice" height="12pt" width="12pt" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
-    <path d="M27 28V0L0.707107 26.2929C0.077142 26.9229 0.523308 28 1.41421 28H27Z"></path>
-    </svg>
+      <svg preserveAspectRatio="XMinYMin slice" height="12pt" width="12pt" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+      <path d="M27 28V0L0.707107 26.2929C0.077142 26.9229 0.523308 28 1.41421 28H27Z"></path>
+      </svg>
     '''
     html = """
-    <!DOCTYPE html/>
-    <html><head><meta charset="utf-8"/> 
-    <style>
+      <!DOCTYPE html/>
+      <html><head><meta charset="utf-8"/> 
+      <style>
     """
     avatar = requests.get(url="https://nivolog.ga/quotes/ava.html").text
     wkoptions = {
-        "transparent":                  "",
-        "enable-local-file-access":     "",
-        "encoding":                     "UTF-8",
-        "zoom":                         "3.0",
-        "xvfb":                         ""
+      "transparent":                  "",
+      "enable-local-file-access":     "",
+      "encoding":                     "UTF-8",
+      "zoom":                         "3.0",
+      "xvfb":                         ""
     }
     wkaoptions = {
       "width":      "512", 
@@ -47,94 +51,97 @@ class quotebuilderMod(loader.Module):
       return avatar
 
     async def client_ready(self, client, db):
-        self.avatar = requests.get(url="https://nivolog.ga/quotes/ava.html").text
-        self.client = client
+      self.avatar = requests.get(url="https://nivolog.ga/quotes/ava.html").text
+      self.client = client
 
     async def quotecmd(self, message):
-        args = utils.get_args_raw(message.message).split()
-        count = int(args[0])
-        html = self.html
-        client = self.client
-        if not message.is_reply:
-          await message.edit("Reply to a message, retard")
-          return
-        else:
-          reply = await message.get_reply_message()
-        await message.edit("generating..")
-        junkfiles = ["quote.png", "quote.webp", "quote.html"]
-        css = requests.get(url="https://nivolog.ga/quotes/quotes.css").text
-        html += css
-        html += "</style></head><body style=\"padding-bottom: 50px;\">\n"
-        html += '<div class="message">\n'
-        fwd = reply.fwd_from
+      """
+      Quote this!
+      """
+      args = utils.get_args_raw(message.message).split()
+      count = int(args[0]) if args else 0
+      html = self.html
+      client = self.client
+      if not message.is_reply:
+        await message.edit("Reply to a message, retard")
+        return
+      else:
+        reply = await message.get_reply_message()
+      await message.edit("Hacking Pentagon..")
+      junkfiles = ["quote.png", "quote.webp", "quote.html"]
+      css = requests.get(url="https://nivolog.ga/quotes/quotes.css").text
+      html += css
+      html += "</style></head><body>\n"
+      html += '<div class="message">\n'
+      fwd = reply.fwd_from
+      fromid = None
+      if fwd:
+        if fwd.from_id:
+          fromid = reply.forward.sender
+        elif fwd.channel_id:
+          fromid = reply.forward.chat
+      else: 
+        fromid = reply.sender
+      fromname = ""
+      try:
+        fromname = fromid.title
+      except AttributeError:
+        fromname = fwd.from_name if fwd and fwd.from_name else fromid.first_name
+      html += f'<div class="sender c{fromid.id % 7 if fromid else 1}">{fromname}</div>\n'
+      if reply.is_reply:
+        tmpr = await reply.get_reply_message()
+        html += f'<div class="reply"></div>'
+      text = "<br/>".join(reply.text.splitlines())
+      html += f'<div class="text">{text}</div>'
+      prevsender = fromid
+      pic = await self.get_avatar(fromid, fromname)
+      messages = await client.get_messages(reply.to_id, min_id=reply.id, limit=count, reverse=True)
+      for msg in messages:
+        fwd = msg.fwd_from
         fromid = None
         if fwd:
           if fwd.from_id:
-            fromid = reply.forward.sender
+            fromid = msg.forward.sender
           elif fwd.channel_id:
-            fromid = reply.forward.chat
+            fromid = msg.forward.chat
         else: 
-          fromid = reply.sender
-        fromname = ""
+          fromid = msg.sender
+        if not (fromid == prevsender):
+          html += self.arrow
+          junkfiles.append(pic)
+          html += f'<img src="{pic}" class="avatar">\n'
+        html += '</div>\n'
+        html += '<div class="message">\n'
         try:
           fromname = fromid.title
         except AttributeError:
           fromname = fwd.from_name if fwd and fwd.from_name else fromid.first_name
+        pic = await self.get_avatar(fromid, fromname)
         html += f'<div class="sender c{fromid.id % 7 if fromid else 1}">{fromname}</div>\n'
-        if reply.is_reply:
-          tmpr = await reply.get_reply_message()
-          html += f'<div class="reply"></div>'
-        text = "<br/>".join(reply.text.splitlines())
+        text = "<br/>".join(msg.text.splitlines())
         html += f'<div class="text">{text}</div>'
         prevsender = fromid
-        pic = await self.get_avatar(fromid, fromname)
-        messages = await client.get_messages(reply.to_id, min_id=reply.id, limit=count, reverse=True)
-        for msg in messages:
-          fwd = msg.fwd_from
-          fromid = None
-          if fwd:
-            if fwd.from_id:
-              fromid = msg.forward.sender
-            elif fwd.channel_id:
-              fromid = msg.forward.chat
-          else: 
-            fromid = msg.sender
-          if not (fromid == prevsender):
-            html += self.arrow
-            junkfiles.append(pic)
-            html += f'<img src="{pic}" class="avatar">\n'
-          html += '</div>\n'
-          html += '<div class="message">\n'
-          try:
-            fromname = fromid.title
-          except AttributeError:
-            fromname = fwd.from_name if fwd and fwd.from_name else fromid.first_name
-          pic = await self.get_avatar(fromid, fromname)
-          html += f'<div class="sender c{fromid.id % 7 if fromid else 1}">{fromname}</div>\n'
-          text = "<br/>".join(msg.text.splitlines())
-          html += f'<div class="text">{text}</div>'
-          prevsender = fromid
-        junkfiles.append(pic)
-        html += f'<img src="{pic}" class="avatar">\n'
-        html += self.arrow
-        html += '</div>\n'
-        html += "</body></html>"
-        open("quote.html", "w").write(html)
-        try:
-          imgkit.from_file("quote.html", "quote.png", options=self.wkoptions)
-        except Exception as e:
-          if not "ProtocolUnknownError" in str(e):
-            raise e
-        img = Image.open("quote.png").convert("RGBA")
-        if (img.size[0] < 513 and img.size[1] < 513):
-          cropimg = img
-        else:
-          cropimg = img.resize((512, int(float(img.size[1]) * float(512 / float(img.size[0])))), PIL.Image.ANTIALIAS)
-        cropimg.save('quote.webp')
-        client = reply.client
-        await message.edit("sending..")
-        await client.send_file(message.to_id, 'quote.webp', reply_to=reply)
-        await message.edit("cleaning up..")
-        for junk in junkfiles:
-          os.remove(junk)
-        await message.delete()
+      junkfiles.append(pic)
+      html += f'<img src="{pic}" class="avatar">\n'
+      html += self.arrow
+      html += '</div>\n'
+      html += "</body></html>"
+      open("quote.html", "w").write(html)
+      try:
+        imgkit.from_file("quote.html", "quote.png", options=self.wkoptions)
+      except Exception as e:
+        if not "ProtocolUnknownError" in str(e):
+          raise e
+      img = Image.open("quote.png").convert("RGBA")
+      if (img.size[0] < 513 and img.size[1] < 513):
+        cropimg = img
+      else:
+        cropimg = img.resize((512, int(float(img.size[1]) * float(512 / float(img.size[0])))), PIL.Image.ANTIALIAS)
+      cropimg.save('quote.webp')
+      client = reply.client
+      await message.edit("Leaking your personal data..")
+      await client.send_file(message.to_id, 'quote.webp', reply_to=reply)
+      await message.edit("Removing OS..")
+      for junk in junkfiles:
+        os.remove(junk)
+      await message.delete()
